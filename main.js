02c6d1f61dbcb77c6959ff527e652f6c1522be09
Ida Tang <idatangxinen@gmail.com>
2023-02-18 20:50:41 +0800

    improve js code, wincount function, add timer

--- main.js
+++ main.js
@@ -1,110 +1,89 @@
 /*----- constants -----*/
-let winCount = 5;
+let count = 0;
+let totalGames = 15;
+let timerId = setInterval(countdown, 1000);
+let timeLeft = 30;
 
-const color = ['RED', 'ORANGE','YELLOW','GREEN', 'BLUE','PURPLE','BLACK','WHITE','PINK','BROWN','GREY']
+/*----- state variables -----*/
 
-let i = Math.floor((Math.random()*color.length));
+const colors = ['RED', 'ORANGE','YELLOW','GREEN', 'BLUE','PURPLE','BLACK','WHITE','PINK','BROWN','GREY']
+let i = Math.floor((Math.random()*colors.length));
 let j = i;
-
-while( i === j) {
-j = Math.floor((Math.random()*color.length));
+while( i === j){
+j = Math.floor((Math.random()*colors.length));
 }
+
 //rizvan suggestion
 
-/* colorCard = [
+/* colorCards = [
     {background:color[i], colorWord: color[j]},
-    {background:color[i], colorWord: color[j]},
-    {background:color[i], colorWord: color[j]},
-    {background:color[i], colorWord: color[j]}, 
+    {background:color[j], colorWord: color[i]},
     ] */
 
 // ernest suggestion
 
-/*----- state variables -----*/
-
-
 /*----- cached elements  -----*/
-const screenOne = document.querySelector('#screenOne');
-const screenTwo = document.querySelector('#screenTwo');
-const screenThree = document.querySelector('#screenThree');
-
-const readyBtn = document.querySelector('#ready');
-const leftBtn = document.querySelector('#buttonone');
-const rightBtn = document.querySelector('#buttontwo');
-
-const startGame = document.querySelector('#startGame');
-const startBtn = document.querySelector('#startagain');
-
-const timer = document.querySelector('#timer');
+let startBtn = document.querySelector('#startgame');
+let gameScreen = document.querySelector('#game');
+let colorCard = document.querySelector('#colorcard');
+let buttonOne = document.querySelector('#buttonone');
+let buttonTwo = document.querySelector('#buttontwo');
+let score = document.querySelector('#score');
+let timer = document.querySelector('#timer');
+let result = document.querySelector('#result');
 
 /*----- event listeners -----*/
-readyBtn.addEventListener('click', changeScreenOne);
-startGame.addEventListener('click', changeScreenTwo);
-leftBtn.addEventListener('click', newCard);
-rightBtn.addEventListener('click', newCard);
+startBtn.addEventListener('click', gameStart);
+startBtn.addEventListener('click', winCount);
+buttonOne.addEventListener('click', winCount);
+buttonTwo.addEventListener('click', winCount);
+buttonOne.addEventListener('click', colorChange);
+buttonTwo.addEventListener('click', colorChange);
 
-// stretch goals: how shall i make use of array to perform same action on different id attributes?
+// how to call two functions once a event is invoked
 
 /*----- functions -----*/
-
-function changeScreenOne(event){
+function gameStart (event){
 event.preventDefault();
-event.target.parentElement.remove();
-screenTwo.style.visibility = 'visible';
+event.target.parentElement.style.display = 'none';
 }
 
-function changeScreenTwo(event){
-    event.preventDefault();
-    event.target.parentElement.remove();
-    event.target.parentNode.style.visibility = 'hidden';
-    screenThree.style.visibility = 'visible'; 
-}
-
-// stretch goals: how shall i update the visibility of the subsequent screen to visible using event.target?
-
-function newCard(event){
+function gameStart (event){
 event.preventDefault();
-event.target.parentElement.remove();
-let createP = document.createElement('p');
-createP.setAttribute('id', 'score');
-createP.innerText = '0/15';
-// find similarities in the code and call it repetitively
-let createDiv = document.createElement('div');
-createDiv.setAttribute('id', 'colorcard' );
-let leftBtn = document.createElement('button');
-let rightBtn = document.createElement('button')
-
-leftBtn.setAttribute('class', 'answerbutton');
-rightBtn.setAttribute('class', 'answerbutton');
-
-createDiv.innerHTML = color[i]
-createDiv.style.backgroundColor = color[j];     
-leftBtn.innerHTML = color[i];
-rightBtn.innerHTML = color[j];
-
-document.querySelector('body').append(createP);
-document.querySelector('body').append(createDiv);
-document.querySelector('body').append(leftBtn, rightBtn);
+event.target.parentElement.style.display = 'none';   
+}
+function winCount (event){
+if (event.target.innerText === colorCard.innerText) {
+count+=1;
+score.innerHTML = `${count} of ${totalGames}`;
+} else {
+score.innerHTML = `${count} of 15`;
+} colorChange();
 }
 
+// how to stop the game when count reach 15
 
-// how to repeat the process everytime player clickes answerbutton?
-// how to compare the results? 
-// once answer compared, restart newCard
 
-//timer function
+function colorChange(){
+colorCard.style.backgroundColor = colors[i];
+colorCard.innerText = colors[i];
+buttonOne.innerText = colors[j]
+buttonTwo.innerText = colors[i]
+}
 
-/* let timerId = setInterval(countdown, 1000);
-let timeLeft = 3;
+// how to call for colorChange everytime player clickes buttonOne or buttonTwo/after compare answer
+
+
+//timer function - reference from https://stackoverflow.com/questions/4435776/simple-clock-that-counts-down-from-30-seconds-and-executes-a-function-afterward
+
+
 function countdown() {
 if (timeLeft == -1) {
-screenThree.style.visibility = 'hidden';
 } else {
-    timer.innerHTML = timeLeft + ' Seconds Remaining';
+    timer.innerText =  `${timeLeft} Seconds Remaining`;
     timeLeft--;
-  }
+clearInterval();
 }
-
-*/
-// reference from https://stackoverflow.com/questions/4435776/simple-clock-that-counts-down-from-30-seconds-and-executes-a-function-afterward
-
+}
+// how to start timer when game screen started
+// how to show result screen once count down is complete
