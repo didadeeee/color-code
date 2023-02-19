1a05267edc2197faf7c0b4b86760d71efd07a1ac
Ida Tang <idatangxinen@gmail.com>
2023-02-19 16:35:36 +0800

    improve css and html interface, js - hide game screen when game ends

--- main.js
+++ main.js
@@ -26,40 +26,39 @@ let buttonTwo = document.querySelector('#buttontwo');
 let score = document.querySelector('#score');
 let timer = document.querySelector('#timer');
 let result = document.querySelector('#result');
-let play = document.querySelector('.musicplay');
-let stop = document.querySelector('.musicstop');
 
 /*----- event listeners -----*/
 startBtn.addEventListener('click', gameStart);
-startBtn.addEventListener('click', winCount);
 buttonOne.addEventListener('click', winCount);
 buttonTwo.addEventListener('click', winCount);
 buttonOne.addEventListener('click', colorChange);
 buttonTwo.addEventListener('click', colorChange);
 
-// how to call two functions once a event is invoked
+// how to stop calling 2 functions for the same event listener by using callback function?
 
 /*----- functions -----*/
 function gameStart (event){
-event.preventDefault();
 event.target.parentElement.style.display = 'none';
+gameScreen.style.display = 'visible';
 }
 
 function gameEnd (){
-console.log('test')
+    gameScreen.style.display = 'none';
+    result.style.display = 'visible';
 }
-// no event associated and have slightly different behaviour
+
+// how to trigger the gamescreen & result screen to be visible/none?
 
 function winCount (event){
 if (event.target.innerText === colorCard.innerText) {
 count+=1;
 score.innerHTML = `${count} of ${totalGames}`;
+} if (count === 15 || timeLeft === 0 ) {
+gameEnd();
 } else {
 score.innerHTML = `${count} of 15`;
 } colorChange();
 }
-
-// how to stop the game when count reach 15
 
 function colorChange(){
 let randomColorOne = Math.floor(Math.random()*colors.length);
@@ -72,6 +71,7 @@ colorCard.innerText = colors[randomColorTwo];
 buttonOne.innerText = colors[randomColorTwo];
 buttonTwo.innerText = colors[randomColorOne];
 }
+// how shall i randomise the position of button one and two?
 
 //timer function - reference from https://stackoverflow.com/questions/4435776/simple-clock-that-counts-down-from-30-seconds-and-executes-a-function-afterward
 
@@ -85,4 +85,3 @@ clearInterval();
 }
 }
 // how to start timer when game screen started
-// how to show result screen once count down is complete
